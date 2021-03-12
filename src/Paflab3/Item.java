116fcfa8ff4a39c2592a1371d59cea193a825c80
Kalhanvithana <kalhandushanka@gmail.com>
2021-03-12 23:34:43 +0530

    added

--- src/Paflab3/Item.java
+++ src/Paflab3/Item.java
@@ -3,6 +3,7 @@ package Paflab3;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.mysql.cj.xdevapi.Statement;
 
@@ -46,8 +47,10 @@ public class Item {
 	 
 	 try 
 	 { 
-	 Class.forName("com.mysql.jdbc.Driver"); 
-	  con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root", ""); 
+		 Class.forName("com.mysql.jdbc.Driver");
+		 
+		
+	  con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/item","root", ""); 
 	 //For testing
 	 System.out.print("Successfully connected"); 
 	 } 
@@ -59,55 +62,96 @@ public class Item {
 	 return con; 
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
-		 
-		if (request.getParameter("itemCode") != null) 
-		{ 
-		 Item itemObj = new Item(); 
-		 itemObj.connect();//For testing the connect method
-		}
-		
-		
-		
-}
 	
 	public String insertItem(String code, String name, String price, String desc)
 	{ 
-	 String output = ""; 
-	try
-	 { 
-	 Connection con = connect(); 
-	 if (con == null) 
-	 { 
-	 return "Error while connecting to the database"; 
-	 } 
-	 // create a prepared statement
-	 String query = " insert into dataTable (`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)"
-	 + " values (?, ?, ?, ?, ?)"; 
-	 
-	 PreparedStatement preparedStmt = con.prepareStatement(query); 
-	 // binding values
-	 preparedStmt.setInt(1, 0); 
-	 preparedStmt.setString(2, code); 
-	 preparedStmt.setString(3, name); 
-	 preparedStmt.setDouble(4, Double.parseDouble(price)); 
-	 preparedStmt.setString(5, desc); 
-	 
-	 
-	 preparedStmt.execute(); 
-	 con.close(); 
-	 output = "Inserted successfully"; 
-	 } 
-	catch (Exception e) 
-	 { 
-	 output = "Error while inserting"; 
-	 System.err.println(e.getMessage()); 
-	 } 
-	return output; 
+		 String output = ""; 
+		try
+		 { 
+		 Connection con = connect(); 
+		 if (con == null) 
+		 { 
+		 return "Error while connecting to the database"; 
+		 } 
+		 // create a prepared statement
+		 String query = " insert into dbs (itemID,itemCode,itemName,itemPrice,itemDesc)" + " values (?, ?, ?, ?, ?)"; 
+		 
+		 PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 // binding values
+		 preparedStmt.setInt(1, 0); 
+		 preparedStmt.setString(2, code); 
+		 preparedStmt.setString(3, name); 
+		 preparedStmt.setDouble(4, Double.parseDouble(price)); 
+		 preparedStmt.setString(5, desc); 
+		 
+		 
+		 preparedStmt.execute(); 
+		 con.close(); 
+		 output = "Inserted successfully"; 
+		 } 
+		catch (Exception e) 
+		 { 
+		 output = "Error while inserting"; 
+		 System.err.println(e.getMessage()); 
+		 } 
+		return output; 
 	}
+
+	public String readItems()
+	{ 
+			 String output = ""; 
+			try
+			 { 
+			 Connection con = connect(); 
+			 if (con == null) 
+			 { 
+			 return "Error while connecting to the database for reading."; 
+			 } 
+			 // Prepare the html table to be displayed
+			 output = "<table border='1'><tr><th>Item Code</th>" 
+			 +"<th>Item Name</th><th>Item Price</th>"
+			 + "<th>Item Description</th>" 
+			 + "<th>Update</th><th>Remove</th></tr>"; 
+			 String query = "select * from dbs"; 
+			 java.sql.Statement stmt = con.createStatement(); 
+			 ResultSet rs = stmt.executeQuery(query); 
+			 // iterate through the rows in the result set
+			 while (rs.next()) 
+			 { 
+			 String itemID = Integer.toString(rs.getInt("itemID")); 
+			 String itemCode = rs.getString("itemCode"); 
+			 String itemName = rs.getString("itemName"); 
+			 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
+			 String itemDesc = rs.getString("itemDesc"); 
+			 // Add a row into the html table
+			 output += "<tr><td>" + itemCode + "</td>"; 
+			 output += "<td>" + itemName + "</td>"; 
+			 output += "<td>" + itemPrice + "</td>"; 
+			 
+			 output += "<td>" + itemDesc + "</td>";
+			 // buttons
+			 output += "<td><input name='btnUpdate' " 
+			 + " type='button' value='Update'></td>"
+			 + "<td><form method='post' action='items.jsp'>"
+			 + "<input name='btnRemove' " 
+			 + " type='submit' value='Remove'>"
+			 + "<input name='itemID' type='hidden' " 
+			 + " value='" + itemID + "'>" + "</form></td></tr>"; 
+			 } 
+			 con.close(); 
+			 // Complete the html table
+			 output += "</table>"; 
+			 } 
+			catch (Exception e) 
+			 { 
+			 output = "Error while reading the items."; 
+			 System.err.println(e.getMessage()); 
+			 } 
+			return output; 
+	}
+	
+
 
 
 }
